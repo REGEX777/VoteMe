85c10d6fc9c2988fff0308e10d51776fded69ffb
regex <aditya777regex@gmail.com>
2022-05-16 13:56:01 +0530

    :(

--- public/scripts/click.js
+++
@@ -1,7 +0,0 @@
-// const upvote = document.getElementById('upvote');
-// const statment = document.getElementById('statement');
-
-// upvote.addEventListener('click', function() {
-//     console.log('clicked');
-//     alert('We are working on it');
-// })
